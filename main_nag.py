fa07314ff7470100b8e842c0bfc2faa2f2790a15
pmartins2106 <125148071+pmartins2106@users.noreply.github.com>
2023-06-22 05:38:58 +0100

    Add files via upload

--- main_nag.py
+++ main_nag.py
@@ -18,18 +18,10 @@ from page_about import page_about
 # from dotenv import load_dotenv
 # import os
 # load_dotenv(".env")    
-import streamlit.components.v1 as components
-
-# Include Google Analytics tracking code
-with open("GA4.html", "r") as f:
-    html_code = f.read()
-    components.html(html_code, height=0)
-
-st.title("My Streamlit App")
 
 
 # Set the default elements on the sidebar
-# st.set_page_config(page_title='NAGpkin')
+st.set_page_config(page_title='NAGpkin')
 
 # analytics
 # streamlit_analytics.start_tracking()
