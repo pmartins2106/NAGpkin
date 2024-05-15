1daa40be6dd2c537d48a3d4ffeb8117055e92ae6
pmartins2106 <125148071+pmartins2106@users.noreply.github.com>
2024-05-15 09:42:32 +0100

    Update main_nag.py

--- main_nag.py
+++ main_nag.py
@@ -14,37 +14,37 @@ import logging
 # Streamlit
 import streamlit as st
 
-# # google analytics
-# def inject_ga():
-#     GA_ID = "google_analytics"
+# google analytics
+def inject_ga():
+    GA_ID = "google_analytics"
 
 
-#     GA_JS = """
-#     <!-- Google tag (gtag.js) -->
-#     <script async src="https://www.googletagmanager.com/gtag/js?id=G-10ZHSZ60NN"></script>
-#     <script>
-#       window.dataLayer = window.dataLayer || [];
-#       function gtag(){dataLayer.push(arguments);}
-#       gtag('js', new Date());
+    GA_JS = """
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-10ZHSZ60NN"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
     
-#       gtag('config', 'G-10ZHSZ60NN');
-#     </script>>
-#     """
+      gtag('config', 'G-10ZHSZ60NN');
+    </script>>
+    """
 
-#     # Insert the script in the head tag of the static template inside your virtual
-#     index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
-#     logging.info(f'editing {index_path}')
-#     soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-#     if not soup.find(id=GA_ID): 
-#         bck_index = index_path.with_suffix('.bck')
-#         if bck_index.exists():
-#             shutil.copy(bck_index, index_path)  
-#         else:
-#             shutil.copy(index_path, bck_index)  
-#         html = str(soup)
-#         new_html = html.replace('<head>', '<head>\n' + GA_JS)
-#         index_path.write_text(new_html)
-# inject_ga()
+    # Insert the script in the head tag of the static template inside your virtual
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    logging.info(f'editing {index_path}')
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=GA_ID): 
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)  
+        else:
+            shutil.copy(index_path, bck_index)  
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + GA_JS)
+        index_path.write_text(new_html)
+inject_ga()
 
 #Theming
 CURRENT_THEME = "Dark"
